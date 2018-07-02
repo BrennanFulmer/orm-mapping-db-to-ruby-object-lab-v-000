68fa17bd2a68afb65ef1d937e76bf2fa26ffb7c6
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-02 17:09:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,12 @@ class Student
   end
 
   def self.count_all_students_in_grade_9
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
   end
 
   def self.all
